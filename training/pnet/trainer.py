6a8f1796abc0fb274befabd2d65fa3119da97c94
zhang jiaqing <54712081+icey-zhang@users.noreply.github.com>
2021-12-24 12:19:23 +0800

    Update trainer.py

--- training/pnet/trainer.py
+++ training/pnet/trainer.py
@@ -7,6 +7,7 @@ setup_logger('base', 'output', 'pnet-train', level=logging.INFO,
                   screen=True, tofile=True)
 logger = logging.getLogger('base')
 from accuracy import compute_accuracy
+import time
 class PNetTrainer(object):
     
     def __init__(self, epochs, dataloaders, model, optimizer, scheduler):
@@ -19,12 +20,13 @@ class PNetTrainer(object):
         
         # save best model
         self.best_val_loss = 100
-        self.start_epoch = 64
-        self.model.set_state_dict(paddle.load('./pretrained_weights/63_pnet.pdparams'))
+        self.best_val_accuracy = 0
+        # self.start_epoch = 64
+        # self.model.set_state_dict(paddle.load('./pretrained_weights/63_pnet.pdparams'))
     
     def train(self):
         paddle.load
-        for epoch in range(self.start_epoch,self.epochs):
+        for epoch in range(self.epochs):#(self.start_epoch,self.epochs):
             self.train_epoch(epoch, 'train')
             self.train_epoch(epoch, 'val')
 
@@ -38,8 +40,20 @@ class PNetTrainer(object):
             self.model.train()
         else:
             self.model.eval()
+        #training log
+        train_reader_cost = 0.0
+        train_run_cost = 0.0
+        total_samples = 0
+        # acc = 0.0
+        reader_start = time.time()
+        batch_past = 0
+        ###############
 
         for batch_idx, sample in enumerate(self.dataloaders[phase]):
+            #training log
+            train_reader_cost += time.time() - reader_start
+            train_start = time.time()
+            ###############
             data = sample['input_img']
             gt_cls = sample['cls_target']
             gt_bbox = sample['bbox_target']
@@ -60,28 +74,50 @@ class PNetTrainer(object):
                 if phase == 'train':
                     total_loss.backward()
                     self.optimizer.step()
+            
+            #training log
+            train_run_cost += time.time() - train_start
+            total_samples += data.shape[0]
+            batch_past += 1
+            ###############
 
             cls_loss_.update(cls_loss, data.shape[0])
             bbox_loss_.update(bbox_loss, data.shape[0])
             total_loss_.update(total_loss, data.shape[0])
             accuracy_.update(accuracy, data.shape[0])
-            
+
+            #training log
             if batch_idx % 40 == 0:
-                logger.info('{} Epoch: {} [{:08d}/{:08d} ({:02.0f}%)]\tLoss: {:.6f} cls Loss: {:.6f} offset Loss:{:.6f}\tAccuracy: {:.6f}'.format( #LR:{:.7f}
+                logger.info('{} Epoch: {} [{:08d}/{:08d} ({:02.0f}%)]\tLoss: {:.6f} cls Loss: {:.6f} offset Loss:{:.6f}\tAccuracy: {:.6f}\tavg_reader_cost: {:.5f} sec, avg_batch_cost: {:.5f} sec, avg_samples: {}, avg_ips: {:.5f} images/sec.'.format( #LR:{:.7f}
                     phase, epoch, batch_idx * len(data), len(self.dataloaders[phase].dataset),
-                    100. * batch_idx / len(self.dataloaders[phase]), total_loss.item(), cls_loss.item(), bbox_loss.item(), accuracy.item())) #, self.optimizer.param_groups[0]['lr']
-        # if epoch % 10 == 0:
-        #     paddle.save(self.model.state_dict(), './pretrained_weights_pnet/{}_pnet.pdparams'.format(epoch))
-        
+                    100. * batch_idx* len(data) / len(self.dataloaders[phase].dataset), total_loss.item(), cls_loss.item(), bbox_loss.item(), accuracy.item(), train_reader_cost / batch_past,
+                    (train_reader_cost + train_run_cost) / batch_past,
+                    total_samples / batch_past,
+                    total_samples / (train_reader_cost + train_run_cost))) #, self.optimizer.param_groups[0]['lr']
+                # logger.info('{} Epoch: {} [{:08d}/{:08d} ({:02.0f}%)]\tLoss: {:.6f} cls Loss: {:.6f} offset Loss:{:.6f}\tAccuracy: {:.6f}'.format(
+                #     phase, epoch, batch_idx * len(data), len(self.dataloaders[phase].dataset),
+                #     100. * batch_idx / len(self.dataloaders[phase]), total_loss.item(), cls_loss.item(), bbox_loss.item(), accuracy.item()))
+
+                train_reader_cost = 0.0
+                train_run_cost = 0.0
+                total_samples = 0
+                # acc = 0.0
+                batch_past = 0
+
+
+            reader_start = time.time()
+            ###############
         if phase == 'train':
             self.scheduler.step()
         
         logger.info("{} epoch Loss: {:.6f} cls Loss: {:.6f} bbox Loss: {:.6f} Accuracy: {:.6f}".format(
             phase, total_loss_.avg.item(), cls_loss_.avg.item(), bbox_loss_.avg.item(), accuracy_.avg.item()))
         
-        if phase == 'val' and total_loss_.avg < self.best_val_loss:
-            self.best_val_loss = total_loss_.avg
+        if phase == 'val' and accuracy_.avg > self.best_val_accuracy:
+            # self.best_val_loss = total_loss_.avg
+            self.best_val_accuracy = accuracy_.avg
             paddle.save(self.model.state_dict(), './weights/best_pnet.pdparams')
+            print("save best weight in epoch {}".format(epoch))
         
         return cls_loss_.avg, bbox_loss_.avg, total_loss_.avg, accuracy_.avg
     
