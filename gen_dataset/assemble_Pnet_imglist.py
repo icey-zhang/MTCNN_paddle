369623c4654b84d97942a61d1c67c00b8799b03a
zhang jiaqing <54712081+icey-zhang@users.noreply.github.com>
2021-12-24 12:15:34 +0800

    Update assemble_Pnet_imglist.py

--- gen_dataset/assemble_Pnet_imglist.py
+++ gen_dataset/assemble_Pnet_imglist.py
@@ -4,23 +4,25 @@ sys.path.append(os.getcwd())
 import assemble
 import config
 
-mode = 'train' #train or val
-net_size = config.PNET_SIZE
 
-pnet_postive_file = 'annotations/pos_{}_{}.txt'.format(net_size, mode)
-pnet_part_file = 'annotations/part_{}_{}.txt'.format(net_size, mode)
-pnet_neg_file = 'annotations/neg_{}_{}.txt'.format(net_size, mode)
-# pnet_landmark_file = './anno_store/landmark_12.txt'
-imglist_filename = 'annotations/imglist_anno_{}_{}.txt'.format(net_size, mode)
 
 if __name__ == '__main__':
+    for mode in ['train','val']:
+        # mode = 'train'
+        net_size = config.PNET_SIZE
 
-    anno_list = []
+        pnet_postive_file = 'annotations/pos_{}_{}.txt'.format(net_size, mode)
+        pnet_part_file = 'annotations/part_{}_{}.txt'.format(net_size, mode)
+        pnet_neg_file = 'annotations/neg_{}_{}.txt'.format(net_size, mode)
+        # pnet_landmark_file = './anno_store/landmark_12.txt'
+        imglist_filename = 'annotations/imglist_anno_{}_{}.txt'.format(net_size, mode)
 
-    anno_list.append(pnet_postive_file)
-    anno_list.append(pnet_part_file)
-    anno_list.append(pnet_neg_file)
-    # anno_list.append(pnet_landmark_file)
+        anno_list = []
 
-    chose_count = assemble.assemble_data(imglist_filename ,anno_list)
-    print("PNet train annotation result file path:%s" % imglist_filename)
+        anno_list.append(pnet_postive_file)
+        anno_list.append(pnet_part_file)
+        anno_list.append(pnet_neg_file)
+        # anno_list.append(pnet_landmark_file)
+
+        chose_count = assemble.assemble_data(imglist_filename ,anno_list)
+        print("PNet train annotation result file path:%s" % imglist_filename)
